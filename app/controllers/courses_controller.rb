38bc8165f27f0573fb98a809b3c1f180a07833ec
pedromcorreia <pedmcor@gmail.com>
2019-08-20 16:05:28 -0300

    Update the resources for courses assoc with school

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,35 +1,45 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
-  # GET /courses
+    # GET /courses
   # GET /courses.json
   def index
-    @courses = Course.all
+    school = School.find(params[:school_id])
+
+    @courses = school.courses
   end
 
   # GET /courses/1
   # GET /courses/1.json
   def show
+    school = School.find(params[:school_id])
+    @courses = school.courses.find(params[:id])
   end
 
   # GET /courses/new
   def new
-    @course = Course.new
+    school = School.find(params[:school_id])
+
+    @course = school.courses.build
   end
 
   # GET /courses/1/edit
   def edit
+    school = School.find(params[:school_id])
+    @courses = school.courses.find(params[:id])
   end
 
   # POST /courses
   # POST /courses.json
   def create
-    @course = Course.new(course_params)
+    school = School.find(params[:school_id])
+
+    @course = school.courses.create(course_params)
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to @course, notice: 'Course was successfully created.' }
-        format.json { render :show, status: :created, location: @course }
+        format.html { redirect_to [@course.school, @school], notice: 'course was successfully created.' }
+        format.json { render :show, status: :created, location: [@course.school, @school] }
       else
         format.html { render :new }
         format.json { render json: @course.errors, status: :unprocessable_entity }
@@ -40,10 +50,14 @@ class CoursesController < ApplicationController
   # PATCH/PUT /courses/1
   # PATCH/PUT /courses/1.json
   def update
+    school = School.find(params[:school_id])
+
+    @course = school.courses.find(params[:id])
+
     respond_to do |format|
-      if @course.update(course_params)
-        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
-        format.json { render :show, status: :ok, location: @course }
+      if @course.update_attributes(course_params)
+        format.html { redirect_to [@course.school, @school], notice: 'course was successfully updated.' }
+        format.json { render :show, status: :ok, location: [@course.school, @school] }
       else
         format.html { render :edit }
         format.json { render json: @course.errors, status: :unprocessable_entity }
@@ -54,9 +68,11 @@ class CoursesController < ApplicationController
   # DELETE /courses/1
   # DELETE /courses/1.json
   def destroy
+    school = School.find(params[:school_id])
+    @course = school.courses.find(params[:id])
     @course.destroy
     respond_to do |format|
-      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
+      format.html { redirect_to school_courses_url, notice: 'course was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
