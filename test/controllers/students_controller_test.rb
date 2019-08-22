acfaf480208236c53775a3236e5ffc0194f8da9c
pedromcorreia <pedmcor@gmail.com>
2019-08-22 12:35:27 -0300

    Add new test in rspec

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -6,43 +6,44 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get students_url
+    @student.inspect
+    get school_students_url
     assert_response :success
   end
 
   test "should get new" do
-    get new_student_url
+    get new_school_student_url
     assert_response :success
   end
 
   test "should create student" do
     assert_difference('Student.count') do
-      post students_url, params: { student: { name: @student.name, school_id: @student.school_id } }
+      post school_students_url, params: { student: { name: @student.name, school_id: @student.school_id } }
     end
 
-    assert_redirected_to student_url(Student.last)
+    assert_redirected_to school_student_url(Student.last)
   end
 
   test "should show student" do
-    get student_url(@student)
+    get school_student_url(@student)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_student_url(@student)
+    get edit_school_student_url(@student)
     assert_response :success
   end
 
   test "should update student" do
-    patch student_url(@student), params: { student: { name: @student.name, school_id: @student.school_id } }
-    assert_redirected_to student_url(@student)
+    patch school_student_url(@student), params: { student: { name: @student.name, school_id: @student.school_id } }
+    assert_redirected_to school_student_url(@student)
   end
 
   test "should destroy student" do
     assert_difference('Student.count', -1) do
-      delete student_url(@student)
+      delete school_student_url(@student)
     end
 
-    assert_redirected_to students_url
+    assert_redirected_to school_students_url
   end
 end
